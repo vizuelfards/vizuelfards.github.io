773fe5b4126f53e9095cc79d9b8fb63ad01e0ed1
unnamed 1924539 <amberisamab@gmail.com>
2025-07-25 13:04:09 +1200

    fard is kinda broken

--- fard.js
+++ fard.js
@@ -2,4 +2,4 @@ let fard = document.getElementById("fard");
 
 console.log(window.location.pathname)
 
-fard.src = `fards/vf_${window.location.pathname.shift(1)}.jpg`
+fard.src = `fards/vf_${window.location.pathname}.jpg`
